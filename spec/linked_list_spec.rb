d299770618ad81ba98bdf2b3746472e872d7fa88
Bradley Milton <bmilton2412@gmail.com>
2023-03-28 15:10:50 -0700

    linked list spec iteration 1

--- spec/linked_list_spec.rb
+++ spec/linked_list_spec.rb
@@ -2,7 +2,20 @@ require 'rspec'
 require './lib/linked_list'
 require './lib/node'
 
-RSpec.describe Linked_list do
-  it 'exist' do 
-    list = LinkedList.new
+RSpec.describe LinkedList do
+  it 'exists' do
+  list = LinkedList.new
+  expect(list).to be_instance_of(LinkedList)
+  expect(list.head).to eq (nil)
+  end
+
+  it 'can append & keep count' do
+  list = LinkedList.new
+  expect(list.append("doop")).to eq("doop")
+  expect(list.head).to be_instance_of(Node)
+  expect(list.head.next_node).to eq(nil)
+  expect(list.count).to eq(1)
+  end
+
+  
 
