2a34de4041efb1b42cb8472f2ebfb8ac78ae6124
Bradley Milton <bmilton2412@gmail.com>
2023-03-28 14:46:37 -0700

    append added to link list class

--- lib/linked_list.rb
+++ lib/linked_list.rb
@@ -1,14 +1,26 @@
-class = LinkedList 
+require './lib/node'
+require 'pry'
 
+class = LinkedList 
+  attr_reader :head
   def initialize
     @head = nil
   end
 
   def append(data)
-    if @head
-      find_tail.next = Node.new(data)
-    else
-      @head = Node.new(data)
+    correct_sound = check_sound(data)
+    if correct_sound == true
+      if @head.nil?                     
+        @head = Node.new(data)        
+        else                              
+        current_node = @head              
+        until current_node.next_node.nil?   
+            current_node = current_node.next_node   
+        end
+          current_node.next_node = Node.new(data)   
+        end
+      data    
+    else "Incorrect sound!"
     end
   end
 
