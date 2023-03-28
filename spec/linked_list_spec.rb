7cb6c46f4bd71fad032bcbecf54156b6c535e85c
Bradley Milton <bmilton2412@gmail.com>
2023-03-28 14:35:26 -0700

    update node spec.rb

---
+++ spec/linked_list_spec.rb
@@ -0,0 +1,8 @@
+require 'rspec'
+require './lib/linked_list'
+require './lib/node'
+
+RSpec.describe Linked_list do
+  it 'exist' do 
+    list = LinkedList.new
+
--- spec/node_spec.rb
+++ spec/node_spec.rb
@@ -7,7 +7,9 @@ RSpec.describe Node do
   it "exists" do
     node = Node.new("plop")
 
+    expect(node).to be_instance_of(Node)
     expect(node.data).to eq("plop")
+    expect(node.next_node).to eq (nil)
   end
 end
 
